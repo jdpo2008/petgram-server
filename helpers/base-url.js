7a01081c049f3b3b87c2a29bf4e9f788d2d0f351
Jose Daniel Perez Ochoa <jperezoch@gp.inet>
2020-08-25 19:53:21 -0500

    Se crea el servidor con email auth02

--- helpers/base-url.js
+++ helpers/base-url.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseUrl:
     process.env.MODE === "PRODUCTION"
-      ? "https://petgram-server.herokuapp.com/"
+      ? "https://sheltered-cove-77224.herokuapp.com/"
       : "http://localhost:5001/",
 };
